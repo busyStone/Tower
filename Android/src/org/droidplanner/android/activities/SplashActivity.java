59688863b9f78b7f8b43e2dbd132a9afb8f906a2
busyStone <jlu.xzp@gmail.com>
2015-10-12 15:27:16 +0800

    [M] Splash 显示版本号

--- Android/src/org/droidplanner/android/activities/SplashActivity.java
+++ Android/src/org/droidplanner/android/activities/SplashActivity.java
@@ -2,7 +2,10 @@ package org.droidplanner.android.activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import org.droidplanner.android.R;
 
@@ -12,6 +15,9 @@ public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        TextView appVersion = (TextView)findViewById(R.id.splash_version);
+        appVersion.setText(getAppVersion());
 
         Thread timerThread = new Thread(){
             public void run(){
@@ -33,5 +39,17 @@ public class SplashActivity extends Activity {
         // TODO Auto-generated method stub
         super.onPause();
         finish();
+    }
+
+    public String getAppVersion(){
+        PackageManager pm = getPackageManager();
+        try {
+            PackageInfo info = pm.getPackageInfo(getPackageName(), 0);
+            return info.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            //不可能发生；
+            return "";
+        }
     }
 }
