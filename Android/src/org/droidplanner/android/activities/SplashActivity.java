8bd8aeb1aa9aca9d84f4d29c3f56153eda482902
busyStone <jlu.xzp@gmail.com>
2015-10-12 10:39:49 +0800

    [A] 增加开机界面

---
+++ Android/src/org/droidplanner/android/activities/SplashActivity.java
@@ -0,0 +1,37 @@
+package org.droidplanner.android.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import org.droidplanner.android.R;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashActivity.this, FlightActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
